a105765a69e946ce795596b1d1d82bdb6284fb83
A7md0h <67323952+A7md0h@users.noreply.github.com>
2024-10-04 23:11:18 +0400

    Update reports.js

--- reports.js
+++ reports.js
@@ -1,10 +1,27 @@
+// استيراد Firebase و Firestore
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+
+// إعداد Firebase
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// تهيئة Firebase
+const app = initializeApp(firebaseConfig);
+
+// الحصول على Firestore
+const db = getFirestore(app);
 
 // تأكيد تحميل الملف
 console.log("تم تحميل reports.js بنجاح");
 
-const db = getFirestore();
-
+// الدالة الخاصة بعرض التقرير الإجمالي
 function showTotalReport() {
     const reportsCollection = collection(db, "studentReports");
     getDocs(reportsCollection).then((querySnapshot) => {
@@ -31,6 +48,7 @@ function showTotalReport() {
     });
 }
 
+// الدالة الخاصة بعرض التقرير حسب الصف
 function showGradeReport() {
     const reportsCollection = collection(db, "studentReports");
     getDocs(reportsCollection).then((querySnapshot) => {
@@ -63,6 +81,7 @@ function showGradeReport() {
     });
 }
 
+// الدالة الخاصة بعرض التقرير المفصل
 function showDetailedReport() {
     const reportsCollection = collection(db, "studentReports");
     getDocs(reportsCollection).then((querySnapshot) => {
